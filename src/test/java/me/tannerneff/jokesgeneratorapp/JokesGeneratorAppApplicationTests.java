3f8623c1f817561d6d705a7413232e4e27d16e46
wneff <wneff.fr@gmail.com>
2020-05-09 07:45:24 -0400

    Initial commit of empty project

---
+++ src/test/java/me/tannerneff/jokesgeneratorapp/JokesGeneratorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tannerneff.jokesgeneratorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesGeneratorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
